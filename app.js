0468d3dbe445b9e90262c9ff3bb5007a9ef05774
Max <nikulins.maksims@gmail.com>
2022-01-29 18:00:19 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,43 @@
+const App = {
+    el: '#app',
+    data() {
+        return {
+            valueInput: '',
+            needDoList: [],
+            completeList: []
+        }
+    },
+    methods: {
+        handleInput (e) {
+            this.valueInput = e.target.value; 
+        },
+        addTask () {
+            if(this.valueInput === '') {return};
+            this.needDoList.push({
+                title: this.valueInput,
+                id: Math.random()
+            });
+            this.valueInput = '';
+        },
+        doCheck (index, type) {
+            if(type === 'need') {
+                const completeMask = this.needDoList.splice(index, 1);
+                this.completeList.push(...completeMask);
+            } else {
+                const notComleteMask = this.completeList.splice(index, 1);
+                this.needDoList.push(...notComleteMask);
+            }
+        },
+        removeMask (index, type) {
+            const toDoList = type === 'need' ? this.needDoList : this.completeList;
+            toDoList.splice(index, 1)
+        }
+    }
+}
+
+
+
+
+const app = Vue.createApp(App)
+
+app.mount('#app')
